a67c022eac8d4f702882fe77407d0eec05532908
nickotronz7 <nickolasrc7@gmail.com>
2016-11-12 14:11:22 -0600

    creacion, lectura y escritura de ficheros

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
+/**
+ * Path : "/home/nickotronz7/ClionProjects/Tec-Media-File-System/"
+ */
+
 #include <iostream>
-#include <libxml/xmlmemory.h>
-#include <libxml/parser.h>
-#include "server.h"
-#include "client.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+using namespace std;
+
+void write(FILE * file);
 
 int main()
 {
-    Server * servidor = new Server();
-    Client * cliente = new Client();
-    //servidor->server();
-    //cliente->client();
+    FILE * pFile;
+    pFile = fopen ("/home/nickotronz7/ClionProjects/Tec-Media-File-System/prueba.txt" , "a+");
+
+    write(pFile);
+
+    fclose(pFile);
 
     return 0;
+}
+
+void write(FILE * file)
+{
+    char mystring [100];
+    printf("Para terminar de introducir texto introdusca unicamente '*' \n");
+    while(1)
+    {
+        printf("Ingrese la linea de texto, (tab equivale a 3 espacios)\n");
+        if (fgets(mystring, 100, stdin) != NULL)
+        {
+            if(mystring[0] == '*')
+            {
+                break;
+            } else {
+                fputs(mystring, file);
+            }
+        }
+    }
+    fputs("\n", file);
 }
