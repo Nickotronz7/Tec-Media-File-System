6ffe10010d4ecd6e3d5b562856b5ee91064d4368
nickotronz7 <nickolasrc7@gmail.com>
2016-11-12 20:25:36 -0600

    añadida biblioteca para el manejo de xml

--- main.cpp
+++ main.cpp
@@ -1,25 +1,67 @@
 /**
- * Path : "/home/nickotronz7/ClionProjects/Tec-Media-File-System/"
+ * Path : "/home/nickotronz7/Desktop/prueba.txt"
  */
 
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
+#include <vector>
+#include "tinyxml2.h"
 
+using namespace tinyxml2;
 using namespace std;
+
+#ifndef XMLCheckResult
+#define XMLCheckResult(a_eResult) if (a_eResult != XML_SUCCESS) { printf("Error: %i\n", a_eResult); return a_eResult; }
+#endif
 
 void write(FILE * file);
 
 int main()
 {
-    FILE * pFile;
-    pFile = fopen ("/home/nickotronz7/ClionProjects/Tec-Media-File-System/prueba.txt" , "a+");
+    FILE * file;
+    file = fopen ("/home/nickotronz7/Desktop/kokkachi.xml", "a+");
+    //write(pFile);
 
-    write(pFile);
+    int myints[] = {20, 40, 7, 12};
+    vector<int> vec (myints, myints + sizeof(myints)/sizeof(int));
 
-    fclose(pFile);
+    XMLDocument xmlDoc;
+    xmlDoc.LoadFile(file);
+    XMLNode * pRoot = xmlDoc.NewElement("Root");
+    xmlDoc.InsertFirstChild(pRoot);
+    XMLElement * pElement = xmlDoc.NewElement("IntValue");
 
+    pElement->SetText(10);
+    pRoot->InsertEndChild(pElement);
+
+    pElement = xmlDoc.NewElement("FloatValue");
+    pElement->SetText(0.5f);
+    pRoot->InsertEndChild(pElement);
+
+    pElement = xmlDoc.NewElement("Date");
+    pElement->SetAttribute("day", 5);
+    pElement->SetAttribute("month", "Febrero");
+    pElement->SetAttribute("year", 1997);
+    pElement->SetAttribute("dateFormat", "5/02/1997");
+    pRoot->InsertEndChild(pElement);
+
+    pElement = xmlDoc.NewElement("List");
+    for (const auto & item : vec)
+    {
+        XMLElement * pListElement = xmlDoc.NewElement("Item");
+        pListElement->SetText(item);
+
+        pElement->InsertEndChild(pListElement);
+    }
+    pElement->SetAttribute("itemCount", 4);
+    pRoot->InsertEndChild(pElement);
+
+    XMLError eResult = xmlDoc.SaveFile(file, true);
+    XMLCheckResult(eResult);
+
+    fclose(file);
     return 0;
 }
 
